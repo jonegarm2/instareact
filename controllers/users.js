2fbd2130d6d94ae04c17e233c27cc5c009239c6d
Jon Garman <jonegarm2@gmail.com>
2018-06-27 12:55:37 -0700

    more additions before styling

--- controllers/users.js
+++ controllers/users.js
@@ -14,5 +14,31 @@ function signup(req, res) {
 
 function login(req, res) {
     console.log(req.body)
-    
+        User.findOne({email: req.body.email}).exec().then(user => {
+            if (!user) return res.status(401).json({err: 'bad credentials'});
+            user.comparePassword(req.body.pw, (err, isMatch) => {
+                if (isMatch) {
+                    var token = createJWT(user);
+                    res.json({token: createJWT(user)});
+                } else {
+                    return res.status(401).json({err: 'bad credentials'});
+                }
+            });
+        }).catch(err => res.status(401).json(err));
 }
+
+ /* ------------ Helper Function ---------- */
+
+ function createJWT(user) {
+     delete user.cart;
+     return jwt.sign(
+         {user}, // data payload
+         SECRET,
+         {expiresIn: '48h'}
+     );
+ }
+
+ module.exports = {
+     signup,
+     login
+ };
